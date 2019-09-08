b2a2bde1de792fa862055a3e144b8ee2623b66a5
Villivateur Von <villivateur@vvzero.com>
2019-09-08 21:11:21 +0800

    add backend

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   vuetify,
